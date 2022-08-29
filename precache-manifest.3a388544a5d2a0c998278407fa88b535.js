6a8cfd37669a3e00ca3e0da6163372fd9d745737
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-08-29 14:24:46 +1000

    Updates

---
+++ precache-manifest.3a388544a5d2a0c998278407fa88b535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239291a70f044e81358ea332cb77f635",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "45e2d2322831831a63e4",
+    "url": "/tabs/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e2d2322831831a63e4",
+    "url": "/tabs/static/js/main.903ab703.chunk.js"
+  },
+  {
+    "revision": "c99201bb389f8c23b139",
+    "url": "/tabs/static/js/runtime-main.e9ac576b.js"
+  }
+]);
